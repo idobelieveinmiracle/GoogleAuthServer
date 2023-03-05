4c71a49102d4079bb98a314ab385461bf25aa608
quochung <quochung1997s@gmail.com>
2023-03-05 13:15:30 +0700

    Update python version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val koinKtorVersion: String by project
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 group = "com.rose"
@@ -20,8 +20,8 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
-tasks.create("stage") {
-    dependsOn("installDist")
+tasks {
+    create("stage").dependsOn("installDist")
 }
 
 repositories {
